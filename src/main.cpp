5c3986be3234c4cb46ef0db6e07436f5edc38fc2
bvh2vgtp-k <kalashnicovp@gmail.com>
2026-05-27 10:23:45 +0500

    fix error handling not displaying msg

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,26 @@
 #include <print>
 #include <exception>
 #include <net/net.hpp>
+#include <vector>
 
 int main(){
     try {
 
     net::Listener srv{8080};
     srv.listen();
+    std::vector<char> buff;
 
     for(;;){
         srv.accept();
         std::println("got connection form {}", srv.get_addr());
+        srv.recv(buff);
+        for(auto i : buff){
+            std::println("{}", i);
+            }
         srv.send("hello");
     }
     } catch(std::exception& e){
-        std::println("Error: ", e.what());
+        std::println("Error: {}", e.what());
     }
 
 }

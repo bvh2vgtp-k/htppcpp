3022f91e0404fe0f81e4709c0b9e96a3017128c7
bvh2vgtp-k <kalashnicovp@gmail.com>
2026-06-18 11:39:19 +0500

    add html test for in main

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,32 @@ int main(){
                 .body("Hello")
                 .build();
         });
+
+        srv.register_handler(http::Method::GET, "/hello", [](){
+            std::string html = R"(
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>My page</title>
+            </head>
+            <body>
+                <h1 class="main-title">hello httpsrv</h1>
+                <p>example of html formated text</p>
+            </body>
+            </html>
+            )";
+
+            return http::Response()
+                .status(http::status_code::OK)
+                .type("text/html; charset=utf-8")
+                .body(html)
+                .build();
+        });
         srv.run();
     } catch(std::exception& e){
         std::println("[FATAL] {}", e.what());
     }
+
+    return 0;
 }
